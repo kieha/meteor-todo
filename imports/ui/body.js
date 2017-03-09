2408746273cea6d676cd24d9f148842a4ed95321
Njeri Kieha <sylvia.kieha@andela.com>
2017-03-09 21:18:13 +0300

    Introducing input forms and events

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,9 +3,29 @@ import { Tasks } from '../api/tasks.js';
 
 import './body.html';
 
-
 Template.body.helpers({
 	tasks() {
-		return Tasks.find({});
+		return Tasks.find({}, { sort: { createdAt: -1 } });
 	}
+});
+
+Template.body.events({
+	"submit .new-task" (event) {
+		
+		// Prevent default browser form submit
+	 	event.preventDefault();
+	 
+	 	// get value from form element
+	 	const target = event.target;
+		const text = target.text.value;
+		
+		// Insert task into collection
+		Tasks.insert({
+			text,
+			 createdAt: new Date()
+		});
+		 
+		// clear input form
+		target.text.value = '';
+	},
 });
